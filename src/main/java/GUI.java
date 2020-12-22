4c205b5a9259a6671915e6b5f0432b5fd94ae020
maciekchylak <56119413+maciekchylak@users.noreply.github.com>
2020-12-22 18:16:27 +0100

    Dodano Comboboxy

--- src/main/java/GUI.java
+++ src/main/java/GUI.java
@@ -1,15 +1,19 @@
 import org.json.simple.JSONObject;
+import org.hamcrest.*;
 
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 
+
 public class GUI {
     String city;
     String location;
     double temp;
+
 
     public GUI() {
         JFrame frame = new JFrame();//creating instance of JFrame
@@ -17,20 +21,23 @@ public class GUI {
         final JLabel locationLabel = new JLabel("Current location: none");
         final JLabel temperatureLabel = new JLabel("Current temperature: none");
 
-        JButton showWeather = new JButton("Show weather");
-        showWeather.addActionListener(new ActionListener() {
+        JButton showWeather_current = new JButton("Show current weather");
+        JButton showWeather_tomorrow = new JButton("Show tomorrow weather");
+        JButton showWeather_week = new JButton("Show week weather");
+
+        showWeather_current.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 Api api = new Api("0e082e7abe9c29697bec4d3e968c1e96");
-                try {
-                    JSONObject weather = api.getWeatherByCity(city, "standard", "pl");
-                    location = weather.get("name").toString();
-                    JSONObject weather_main = (JSONObject) weather.get("main");
-                    temp = Double.parseDouble(weather_main.get("temp").toString());
-                } catch (IOException ioException) {
-                    ioException.printStackTrace();
-                }
-                locationLabel.setText("Current location: " + location);
-                temperatureLabel.setText("Current temperature: " + temp);
+               // try {
+                   // JSONObject weather = api.getWeatherByCity(city, "standard", "pl");
+                   // location = weather.get("name").toString();
+                   // JSONObject weather_main = (JSONObject) weather.get("main");
+                   // temp = Double.parseDouble(weather_main.get("temp").toString());
+               // } catch (IOException ioException) {
+               //     ioException.printStackTrace();
+                //}
+               // locationLabel.setText("Current location: " + location);
+                //temperatureLabel.setText("Current temperature: " + temp);
             }
         });
 
@@ -41,10 +48,47 @@ public class GUI {
             }
         });
 
+
+        //Combobox units
+        String[] units = {
+                "pl",
+                "en",
+        };
+        JComboBox combobox_units= new JComboBox(units);
+        combobox_units.setEditable(true);
+        combobox_units.setSelectedIndex(1);
+        combobox_units.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                Api api = new Api("0e082e7abe9c29697bec4d3e968c1e96");
+            }
+            });
+
+        //Combobox languages
+        String[] languages = {
+                "standard",
+                "metric",
+                "imperial",
+
+        };
+        JComboBox combobox_lang = new JComboBox(languages);
+        combobox_lang.setEditable(true);
+        combobox_lang.setSelectedIndex(1);
+        combobox_lang.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                Api api = new Api("0e082e7abe9c29697bec4d3e968c1e96");
+            }
+            });
+
+
+
         JPanel panel = new JPanel();
         panel.setBorder(BorderFactory.createEmptyBorder(300, 300, 100, 300));
         panel.setLayout(new GridLayout(0, 1));
-        panel.add(showWeather);
+        panel.add(showWeather_current);
+        panel.add(combobox_lang);
+        panel.add(combobox_units);
+        panel.add(showWeather_tomorrow);
+        panel.add(showWeather_week);
         panel.add(locationLabel);
         panel.add(cityInput);
         panel.add(temperatureLabel);
@@ -57,19 +101,12 @@ public class GUI {
         frame.setVisible(true);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException, ClassNotFoundException {
+        System.setProperty(
+                "Quaqua.design","lion"
+        );
+
+        UIManager.setLookAndFeel("ch.randelshofer.quaqua.QuaquaLookAndFeel");
         new GUI();
-//        JFrame f= new JFrame();//creating instance of JFrame
-//        JLabel l1,l2;
-//        l1 = new JLabel("Pada");
-//        l1.setBounds(130,50,200, 40);
-//        JButton b= new JButton("pokaz pogode");//creating instance of JButton
-//        b.setBounds(130,100,100, 40);//x axis, y axis, width, height
-//        f.add(l1);
-//        f.add(b);//adding button in JFrame
-//
-//        f.setSize(400,500);//400 width and 500 height
-//        f.setLayout(null);//using no layout managers
-//        f.setVisible(true);//making the frame visible
     }
 }
