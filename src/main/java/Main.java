42870c0dcb5994cc95d5ea111b6c5ca6b2d79f57
gryszkiewiczm <01143557@pw.edu.pl>
2020-12-23 14:47:29 +0100

    zmergowano branch GUIv2 i komunikacja_z_api_v2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,9 +7,9 @@ public class Main {
     public static void main(String [] args) throws IOException {
         Api api = new Api("0e082e7abe9c29697bec4d3e968c1e96");
         System.out.println(api.getWeatherByCity("Warszawa", "standard", "pl"));
-        JSONObject Warszawa_pogoda = api.getWeatherByCity("Warszawa", "standard", "pl");
-        JSONObject Warszawa_pogoda_main = (JSONObject) Warszawa_pogoda.get("main");
-        System.out.println(Warszawa_pogoda.get("name").toString());
-        System.out.println(Double.parseDouble(Warszawa_pogoda_main.get("temp").toString()));
+//        JSONObject Warszawa_pogoda = api.getWeatherByCity("Warszawa", "standard", "pl");
+//        JSONObject Warszawa_pogoda_main = (JSONObject) Warszawa_pogoda.get("main");
+//        System.out.println(Warszawa_pogoda.get("name").toString());
+//        System.out.println(Double.parseDouble(Warszawa_pogoda_main.get("temp").toString()));
     }
 }
